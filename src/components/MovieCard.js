30a16d1402ee4e2916e9aa1032c60aa821faad89
erol <hurkanrol15@gmail.com>
2022-09-06 09:13:18 +0200

    edited navbar

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -2,6 +2,16 @@ import React from 'react';
 
 const IMG_API = "https://image.tmdb.org/t/p/w1280";
 
+const setVoteClass = (vote) => {
+  if (vote >= 8){
+    return 'green';
+  } else if (vote >= 6){
+    return 'orange';
+  } else {
+    return 'red';
+  }
+}
+
 const MovieCard = ({title, poster_path, overview, vote_average}) => {
 
   return (
@@ -9,7 +19,7 @@ const MovieCard = ({title, poster_path, overview, vote_average}) => {
         <img src={IMG_API + poster_path} alt="movie poster" />
         <div className="movie-info">
             <h3>{title}</h3>
-            <span>{vote_average}</span>
+            <span className={`tag ${setVoteClass(vote_average)}`}>{vote_average}</span>
         </div>
         <div className="movie-over">
             <h2>Overview</h2>

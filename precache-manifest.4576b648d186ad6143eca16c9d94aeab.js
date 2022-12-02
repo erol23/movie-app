82813d2fd2fc0f3ce047e512558cbcf7a6c8e69f
erol <hurkanrol15@gmail.com>
2022-12-02 22:06:36 +0100

    Updates

---
+++ precache-manifest.4576b648d186ad6143eca16c9d94aeab.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "f35a8fd90056e1c100f9",
+    "url": "/movie-app/static/css/main.983d5dee.chunk.css"
+  },
+  {
+    "revision": "f35a8fd90056e1c100f9",
+    "url": "/movie-app/static/js/main.f35a8fd9.chunk.js"
+  },
+  {
+    "revision": "dcb7df9bbfde9dd1c6b0",
+    "url": "/movie-app/static/js/1.dcb7df9b.chunk.js"
+  },
+  {
+    "revision": "4847d6f1b222811d6b42",
+    "url": "/movie-app/static/css/2.3ac74afe.chunk.css"
+  },
+  {
+    "revision": "4847d6f1b222811d6b42",
+    "url": "/movie-app/static/js/2.4847d6f1.chunk.js"
+  },
+  {
+    "revision": "199308e96bdbbbd88045",
+    "url": "/movie-app/static/js/runtime~main.199308e9.js"
+  },
+  {
+    "revision": "f447fc3ae194d469a33052d4424a8530",
+    "url": "/movie-app/index.html"
+  }
+];
